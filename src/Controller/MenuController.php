36bbd0bffa2fdeaf2af223b13fc7a2454843f574
PFD <dencaussepaulfranck@hotmail.com>
2025-01-11 17:49:31 +0100

    menu dynamique

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,32 +10,46 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'app_menu_index')]
-    public function index(): Response
+    public function index(MenuRepository $menuRepository): Response
     {
+        // Récupérer tous les éléments du menu depuis la base de données
+        $menus = $menuRepository->findAll();
+
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+            'menus' => $menus,
         ]);
     }
 
     #[Route('/ruMenu', name: 'app_menu_russian')]
-    public function russian(): Response
+    public function russian(MenuRepository $menuRepository): Response
     {
+        // Récupérer tous les éléments du menu pour la version russe
+        $menus = $menuRepository->findAll();
+
         return $this->render('menu/russian.html.twig', [
-            'controller_name' => 'MenuController',
+            'menus' => $menus,
         ]);
     }
+
     #[Route('/enMenu', name: 'app_menu_english')]
-    public function english(): Response
+    public function english(MenuRepository $menuRepository): Response
     {
+        // Récupérer tous les éléments du menu pour la version anglaise
+        $menus = $menuRepository->findAll();
+
         return $this->render('menu/englishMenu.html.twig', [
-            'controller_name' => 'MenuController',
+            'menus' => $menus,
         ]);
     }
+
     #[Route('/esMenu', name: 'app_menu_castellano')]
-    public function castellano(): Response
+    public function castellano(MenuRepository $menuRepository): Response
     {
+        // Récupérer tous les éléments du menu pour la version espagnole
+        $menus = $menuRepository->findAll();
+
         return $this->render('menu/castellanoMenu.html.twig', [
-            'controller_name' => 'MenuController',
+            'menus' => $menus,
         ]);
     }
 }
